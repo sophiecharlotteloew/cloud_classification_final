d835ab9bf52b4cbfa75a0da4ba635e5db3715439
Sophie Charlotte Loew <114905763+sophiecharlotteloew@users.noreply.github.com>
2022-12-07 16:20:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -73,11 +73,11 @@ if uploaded_file is not None:
             my_bar.progress((i+1)*10)
             time.sleep(1)
         
-        txt = st.text_area('Text to analyze', '''Analysis of weather performed! ⛅ ''')
-        st.write('Sentiment:', run_sentiment_analysis(txt))
-        
         st.info('Analysis of weather performed!', icon="⛅")
         st.write(f'### {res}')
+        
+        txt = st.text_area(f'### {res}')
+        st.write('Sentiment:', run_sentiment_analysis(txt))
 
 if uploaded_file_2 is not None:
     
